23becf3543124c6d425bcd285bc8e47d89c5bb0b
minhajputhiyara <minhajputhiyara@gmail.com>
2025-08-13 20:08:44 +0530

    certificate router change

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Desktop } from "./screens/Desktop";
 import { Gallery } from "./screens/Gallery";
 import { Certifications } from "./screens/Certifications";
